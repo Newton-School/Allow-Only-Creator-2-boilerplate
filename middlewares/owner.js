9c75b8d9c4dbd320ef264d0a32bff987165c23cc
swaraj-jain <happy2000jain@gmail.com>
2023-04-19 02:08:02 +0530

    boiler plate added

---
+++ middlewares/owner.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const discussions = JSON.parse(fs.readFileSync(`data/discussions.json`));
+
+/*
+
+Complete the middleware isOwner
+
+which will move further only if the creator_id of the request discussion is same as request_by in req.body
+API /api/v1/discussions/:id --> here id is id of discussion
+req.body = {
+    request_by : request_by
+}
+
+Response:
+
+1. discussion with given id doesnot exist --> 
+
+404 Status code 
+json = {
+    status: "Failed",
+    message: "Discussion not found!",
+}
+
+2. requested discussion creator_id does not match with request_by in request body
+403 Status code 
+json = {
+    status: "Failed",
+    message: "Access Denied",
+}
+
+3. Success -> Next.
+
+*/
+
+function isOwner() {
+    try {
+        return function (req, res, next) {
+
+            const { request_by } = req.body;
+
+            //Write your code here.
+            
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            status: "Failed",
+            message: "Unable to check access level"
+        })
+    }
+}
+
+module.exports = { isOwner };
---
+++ src/app.js
@@ -0,0 +1,127 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+// Importing discussions from discussions.json file
+const discussions = JSON.parse(fs.readFileSync(`data/discussions.json`));
+const { isOwner } = require('../middlewares/owner')
+
+// Middlewares
+app.use(express.json());
+
+
+app.get("/api/v1/discussions", (req, res) => {
+
+    res.status(200).json({
+        status: "Success",
+        message: "Discussions fetched successfully",
+        data: {
+          discussions,
+        },
+      });
+
+});
+
+
+app.get("/api/v1/discussions/:id", (req, res) => {
+
+  const id = req.params.id * 1;
+  const Details = discussions.find(
+    (Details) => Details.id === id
+  );
+
+  const index = discussions.indexOf(Details);
+
+  if (!Details) {
+    return res.status(404).send({
+      status: "Failed",
+      message: "Discussion not found!",
+    });
+  }
+
+  res.status(200).json({
+        status: "Success",
+        data: Details
+  });
+
+});
+
+
+
+app.post("/api/v1/discussions", (req, res) => {
+
+  var obj = req.body;
+  obj['id'] = (discussions[discussions.length-1].id)+1;
+  discussions.push(obj);
+
+  fs.writeFile(
+    `data/discussions.json`,
+    JSON.stringify(discussions),
+    (err) => {
+      res.status(200).json({
+        status: "Success",
+        message: "Discussions added successfully"
+      })
+    }
+  );
+
+});
+
+app.patch("/api/v1/discussions/:id", isOwner(),  (req, res) => {
+
+  const id = req.params.id * 1;
+  const updatedDetails = discussions.find(
+    (updatedDetails) => updatedDetails.id === id
+  );
+
+  const index = discussions.indexOf(updatedDetails);
+
+  if (!updatedDetails) {
+    return res.status(404).send({
+      status: "Failed",
+      message: "Discussion not found!",
+    });
+  }
+
+  Object.assign(updatedDetails, req.body);
+
+  fs.writeFile(
+    `data/discussions.json`,
+    JSON.stringify(discussions),
+    (err) => {
+      res.status(200).json({
+        status: "Success",
+        message: `Discussions Updated Successfully`
+      });
+    }
+  );
+});
+
+
+app.delete("/api/v1/discussions/:id", isOwner(), (req, res) => {
+  const id = req.params.id * 1;
+  const DetailToDelete = discussions.find(
+    (detailTODelete) => detailTODelete.id === id
+  );
+  if (!DetailToDelete) {
+    return res.status(404).send({
+      status: `Failed`,
+      message: `Discussion not found!`,
+    });
+  }
+  const index = discussions.indexOf(DetailToDelete);
+  discussions.splice(index, 1);
+  fs.writeFile(
+    `data/discussions.json`,
+    JSON.stringify(discussions),
+    (err) => {
+      res.status(200).json({
+        status: `Success`,
+        message: `Discussions Deleted Successfully`
+      });
+    }
+  );
+});
+
+
+module.exports = app;
